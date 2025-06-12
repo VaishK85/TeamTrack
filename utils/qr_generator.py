24a0e1c907e3d4b378eb775b93558fa71a0c4f9d
Vaishnavi Kothe <vaishnavikothe8@gmail.com>
2025-06-12 11:44:07 +0530

    Create qr_generator.py
    This utility module is responsible for generating a QR code using the information provided by the Employee object. It uses the qrcode library to encode the employee data into a visually scannable QR format, making the application suitable for digital records and touchless identification systems.

---
+++ utils/qr_generator.py
@@ -0,0 +1,16 @@
+# utils/qr_generator.py
+
+import qrcode
+
+def generate_qr(emp):
+    data = emp.info()
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    return img
